6d3a0b0c543eae749b474f0686073d7d06418194
Eder Dias <ederxdias98@gmail.com>
2022-10-26 08:32:11 -0300

    test

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	gcc -o prog main.c terreno.c circulo.c retangulo.c triangulo.c -lm
--- circulo.h
+++
@@ -1,9 +0,0 @@
-#ifndef Circulo_H
-#define Circulo_H
-typedef struct tCirculo tCirculo;
-tCirculo *CriarCirculo(float raio);
-float AreaCircular(float raio);
-float RetornarAreaC(tCirculo *c);
-void LiberarC(void *p);
-int Tc() ;
-#endif
