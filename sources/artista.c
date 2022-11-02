e13f8b98ce8eddca9f12151f680d332206371c97
Eder Dias <ederxdias98@gmail.com>
2022-11-02 10:01:18 -0300

    commit(eder)

--- sources/artista.c
+++ sources/artista.c
@@ -10,8 +10,35 @@ struct tArtista
     int playP;
 };
 
-tArtista *Iniciarartista
+tArtista *IniciarArtista(){
+    tArtista *art = malloc(sizeof(art));
+    if(art == NULL){
+        printf("Falha na alocação do ponteiro de artista");
+        exit(-1);
+    }
+    return art;
+}
 
 tArtista *LeArtista(FILE *arq, tArtista *art){
+    art->id = malloc(10000);
+    fscanf(arq,"%[^;];", art->id);
+    fscanf(arq,"%f;",art->seg);
+    art->gen = malloc(10000);
+    fscanf(arq,"%[^;];",art->gen);
+    art->nA = malloc(10000);
+    fscanf(arq,"%[^;];", art->nA);
+    fscanf(arq,"%d\n",art->popu);
+}
 
+void LiberarArtistaStr(tArtista *p){
+    if(p != NULL)
+        free(p);
+}
+void LiberaArtistaVet(tArtista *p){
+    if(p->id!=NULL)
+        free(p->id);
+    if(p->gen!=NULL);
+        free(p->gen);
+    if (p->nA!=NULL)
+        free(p->nA);
 }
