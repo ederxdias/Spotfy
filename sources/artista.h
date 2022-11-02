08be4e999ab48b98c0aac5c5843dc619a479a53e
AlanT <alanz4.tdc@gmail.com>
2022-11-02 09:06:42 -0300

    tArtistas

--- sources/artista.h
+++ sources/artista.h
@@ -1,6 +1,10 @@
 #ifndef ARTISTA_H
 #define ARTISTA_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct tArtista tArtista;
 
 #endif
--- sources/artistas.h
+++ sources/artistas.h
@@ -1,6 +1,10 @@
 #ifndef ARTISTAS_H
 #define ARTISTAS_H
 
+#include "artista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct tArtistas tArtistas;
 
 #endif
