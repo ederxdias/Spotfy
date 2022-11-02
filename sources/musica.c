861a0509fa482795f9c6c19631ee01043516c84b
AlanT <alanz4.tdc@gmail.com>
2022-11-02 10:43:31 -0300

    tMusica leitura

--- sources/musica.c
+++ sources/musica.c
@@ -5,18 +5,24 @@ struct tMusica
     char *id;
     char *nM; //Nome da musica
     int popu;
-    float dura_ms;
+    double dura_ms;
     int eh_explic;
     tArtistas *artista; //Vetor de artistas que cantam a musica
     int qtdA; //Quantidade de artistas que produziram a musica
     char **idA; //Id dos artistas que cantam a musica
     char *nA; //Nome dos artistas que cantam a musica
     tData_pt dataL; //Data de lancamento
-    tPropiedades* prop; //Proriedades da musica
+    tPropiedades* prop; //Propriedades da musica
     int qtdPlay; //Quantidade de playlist que pertence
 };
 
-tMusica *CriaMusica(char* id, char* nM, int popu, float duracao, int exp, tArtistas* art, int qtdA, char* nA, char **idA, 
+struct tMusicaVet{
+    tMusica* *musicas;
+    int qtdM;
+    int tamVet;
+};
+
+tMusica *CriaMusica(char* id, char* nM, int popu, double duracao, int exp, tArtistas* art, int qtdA, char* nA, char **idA, 
                     tData_pt dataL, tPropiedades* p,  int qtdPlay) {
     
     tMusica *m = (tMusica*)malloc(sizeof(*m));
@@ -52,4 +58,37 @@ tMusica *CriaMusica(char* id, char* nM, int popu, float duracao, int exp, tArtis
     m->qtdPlay = qtdPlay;
 
     return m;
+}
+
+
+tMusicaVet* InicializaVetorMusicas() {
+    tMusicaVet* v = (tMusicaVet*)malloc(sizeof(*v));
+    v->tamVet = 10;
+    v->musicas = (tMusica**)malloc(sizeof(tMusica*)*v->tamVet);
+    v->qtdM = 0;
+
+    return v;
+}
+
+tMusicaVet* LeMusicasDoArquivo(char* caminho) {
+   tMusicaVet* v = InicializaVetorMusicas;
+
+    char id[100], nM[10000], nA[10000], idA[10000];
+    int popu, exp, qtdPlay = 0, dia, mes, ano;
+    double danceability, energy, speechiness, acousticness;
+    double instrumentalness, liveness, valence, loudness, tempo;
+    double duracao;
+    tPropiedades* p;
+    tData_pt data;
+
+    FILE * f = NULL;
+
+    f = fopen(caminho, "r");
+
+    if(f == NULL){
+        printf("Erro ao abrir arquivo: %s\n", caminho);
+        exit(1);
+    }
+
+
 }
