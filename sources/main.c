b70d6f1567046c3de56876a5d5b9daf90fa8037c
Eder Dias <ederxdias98@gmail.com>
2022-11-02 12:16:45 -0300

    commit(eder)

--- sources/main.c
+++ sources/main.c
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[]) {
 
     FILE * test = fopen("./data_tests/artists_2.csv","r");
+    if (test == NULL)
+  {
+    printf("Erro abertura do arquivo!!");
+    exit(EXIT_FAILURE);
+  }
     tArtistas *arts = CriarArtistas();
     while(1){
         tArtista *art = LeArtista(test);
