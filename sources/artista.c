5ffb0a921f10edefd2dc25cfaeaa208968c865bc
Eder Dias <ederxdias98@gmail.com>
2022-11-02 10:29:03 -0300

    commit(eder)

--- sources/artista.c
+++ sources/artista.c
@@ -10,24 +10,36 @@ struct tArtista
     int playP;
 };
 
-tArtista *IniciarArtista(){
-    tArtista *art = malloc(sizeof(art));
+tArtista *CriarArtista(char *id, double seg, char *gen, char *nA, int popu){
+    tArtista *art =(tArtista*) malloc(sizeof(*art));
     if(art == NULL){
         printf("Falha na alocação do ponteiro de artista");
         exit(-1);
     }
+    art->id = (char*)malloc(sizeof(char)*(strlen(id)+1));
+    strcpy(art->id, id);
+    art->seg = seg;
+    art->gen = (char*)malloc(sizeof(char)*(strlen(gen)+1));
+    strcpy(art->gen, gen);
+    art->nA = (char*)malloc(sizeof(char)*(strlen(nA)+1));
+    strcpy(art->nA, nA);
+    art->popu = popu;
     return art;
 }
 
 tArtista *LeArtista(FILE *arq, tArtista *art){
-    art->id = malloc(10000);
-    fscanf(arq,"%[^;];", art->id);
-    fscanf(arq,"%f;",art->seg);
-    art->gen = malloc(10000);
-    fscanf(arq,"%[^;];",art->gen);
-    art->nA = malloc(10000);
-    fscanf(arq,"%[^;];", art->nA);
-    fscanf(arq,"%d\n",art->popu);
+    char id[10000];
+    fscanf(arq,"%[^;];",id);
+    double seg;
+    fscanf(arq,"%f;",seg);
+    char gen[10000];
+    fscanf(arq,"%[^;];",gen);
+    char nA[10000]; 
+    fscanf(arq,"%[^;];",nA);
+    int popu;
+    fscanf(arq,"%d\n",popu);
+    tArtista *art = CriarArtista(id,seg,gen,nA,popu);
+    return art;
 }
 
 void LiberarArtistaStr(tArtista *p){
